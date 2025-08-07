02484a99849c3d9b85c1e346187be1a4ae447915
Hung Nguyen <nd.hung@me.com>
2025-08-07 08:12:00 +0200

    feat: enhance sign-up page with background image and overlay

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,9 +1,20 @@
 import { SignUpForm } from "@/components/sign-up-form"
+import Image from "next/image"
 
 export default function Page() {
   return (
-    <div className="min-h-screen items-center m-auto flex justify-center">
+    <div className="min-h-screen items-center m-auto flex justify-center relative overflow-hidden">
       <SignUpForm />
+      <Image
+        src="/images/l3.jpg"
+        alt="Platform Kit Logo"
+        fill
+        quality={100}
+        priority
+        className="object-top object-cover  -z-10"
+      />
+
+      <div className="absolute inset-0 bg-black/30 -z-10" />
     </div>
   )
 }
